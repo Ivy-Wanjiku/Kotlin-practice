7a083a66d9b8a5eebd9c1e4b753d2eab0571fa47
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-03-19 07:29:02 +0300

    add files

---
+++ main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(){
+    println(add(5, 9))
+getStringLength("hi")
+    printFullName("Ivy","Wanjiku")
+var x =division(19.0 ,2)
+    println(x)
+    var w= getStringLength("hello")
+    println(w)
+    var q=division (5.0 ,7)
+}
+fun add(num1:Int, num2: Int) :Int {
+    var results = num1 / num2
+    return results
+}
+fun getStringLength(word :String) :Int{
+    return word.length
+}
+
+fun printFullName(firstname: String, lastname: String){
+    var fullname=firstname + "" +lastname
+    println(fullname)
+}
+fun division (num1: Double, num2: Int):Double {
+    var result = num1 / num2
+    return (result)
+
+
+}
+
+
+
+
